2128f8b3c2189ea6119cb01890d867cdf54dea73
Piotr Tarsa <piotr.tarsa@gmail.com>
2017-02-26 00:53:04 +0100

    Implemented caching in TarsaMatchFinder

--- src/main/scala/pl/tarsa/matchfinders/finders/TarsaMatchFinder.scala
+++ src/main/scala/pl/tarsa/matchfinders/finders/TarsaMatchFinder.scala
@@ -33,6 +33,8 @@ object TarsaMatchFinder extends MatchFinder {
     new Engine(inputData, minMatch, maxMatch, onAccepted, onDiscarded).result()
   }
 
+  private val CachedRadixSearchThreshold = 1234
+
   private class Engine(inputData: Array[Byte],
                        minMatch: Int,
                        maxMatch: Int,
@@ -42,9 +44,25 @@ object TarsaMatchFinder extends MatchFinder {
       inputData.length
 
     private val suffixArray =
-      Array.tabulate[Int](size)(identity)
+      Array.ofDim[Int](size)
     private val suffixArrayAuxiliary =
       Array.ofDim[Int](size)
+
+    for (index <- 0 until size) {
+      suffixArray(index) = index
+    }
+
+    private val backColumn =
+      Array.ofDim[Byte](size)
+    private val backColumnAuxiliary =
+      Array.ofDim[Byte](size)
+
+    for (index <- 1 until size) {
+      backColumn(index) = inputData(index - 1)
+    }
+
+    private val activeColumn =
+      Array.ofDim[Byte](size)
 
     private val histogram =
       Array.ofDim[Int](256)
@@ -56,6 +74,97 @@ object TarsaMatchFinder extends MatchFinder {
 
     private def getValue(index: Int, depth: Int): Int = {
       inputData(suffixArray(index) + depth) & 0xFF
+    }
+
+    private def radixSearchCached(lcpLength: Int,
+                                  startingIndex: Int,
+                                  unsafeElementsNumber: Int): Unit = {
+      if (unsafeElementsNumber < CachedRadixSearchThreshold) {
+        radixSearch(lcpLength, startingIndex, unsafeElementsNumber)
+      } else if (lcpLength < maxMatch) {
+        val elementsNumber = {
+          val lastIndex = startingIndex + unsafeElementsNumber - 1
+          if (suffixArray(lastIndex) + lcpLength == size) {
+            if (lcpLength >= minMatch && startingIndex != lastIndex) {
+              val optimalMatch =
+                makePacked(suffixArray(lastIndex - 1),
+                           suffixArray(lastIndex),
+                           lcpLength)
+              if (suffixArray(lastIndex - 1) > 0 && suffixArray(lastIndex) > 0
+                  && backColumn(lastIndex - 1) == backColumn(lastIndex)) {
+                onDiscarded(optimalMatch)
+              } else {
+                onAccepted(optimalMatch)
+              }
+            }
+            unsafeElementsNumber - 1
+          } else {
+            unsafeElementsNumber
+          }
+        }
+        for (index <- startingIndex until startingIndex + elementsNumber) {
+          activeColumn(index) = inputData(suffixArray(index) + lcpLength)
+        }
+        util.Arrays.fill(histogram, 0)
+        for (index <- startingIndex until startingIndex + elementsNumber) {
+          histogram(activeColumn(index) & 0xFF) += 1
+        }
+        if (lcpLength >= minMatch) {
+          for (index <- startingIndex + 1 until
+                 startingIndex + elementsNumber) {
+            val optimalMatch =
+              makePacked(suffixArray(index - 1), suffixArray(index), lcpLength)
+            if (lcpLength < maxMatch &&
+                activeColumn(index - 1) == activeColumn(index)) {
+              onDiscarded(optimalMatch)
+            } else if (suffixArray(index - 1) > 0 && suffixArray(index) > 0 &&
+                       backColumn(index - 1) == backColumn(index)) {
+              onDiscarded(optimalMatch)
+            } else {
+              onAccepted(optimalMatch)
+            }
+          }
+        }
+        destinations(0) = startingIndex
+        for (i <- destinations.indices.tail) {
+          destinations(i) = destinations(i - 1) + histogram(i - 1)
+        }
+        for (index <- startingIndex until startingIndex + elementsNumber) {
+          val value = activeColumn(index) & 0xFF
+          suffixArrayAuxiliary(destinations(value)) = suffixArray(index)
+          backColumnAuxiliary(destinations(value)) = backColumn(index)
+          destinations(value) += 1
+        }
+        Array.copy(suffixArrayAuxiliary,
+                   startingIndex,
+                   suffixArray,
+                   startingIndex,
+                   elementsNumber)
+        Array.copy(backColumnAuxiliary,
+                   startingIndex,
+                   backColumn,
+                   startingIndex,
+                   elementsNumber)
+        val segments = segmentsStack(lcpLength)
+        segments(0) = startingIndex
+        for (i <- segments.indices.tail) {
+          segments(i) = segments(i - 1) + histogram(i - 1)
+        }
+        for (i <- 0 until 256) {
+          val segmentLength = segments(i + 1) - segments(i)
+          if (segmentLength > 1) {
+            radixSearchCached(lcpLength + 1, segments(i), segmentLength)
+          }
+        }
+      } else {
+        assert(lcpLength == maxMatch)
+        for (index <- startingIndex + 1 until
+               startingIndex + unsafeElementsNumber) {
+          val optimalMatch =
+            makePacked(suffixArray(index - 1), suffixArray(index), lcpLength)
+          onAccepted(optimalMatch)
+        }
+      }
     }
 
     private def radixSearch(lcpLength: Int,
@@ -139,7 +248,7 @@ object TarsaMatchFinder extends MatchFinder {
     }
 
     def result(): Unit =
-      radixSearch(0, 0, size)
+      radixSearchCached(0, 0, size)
   }
 
   private def makePacked(source: Int, target: Int, length: Int): Match.Packed =
