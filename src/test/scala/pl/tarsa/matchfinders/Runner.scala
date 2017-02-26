9e72ed3a8818e02bb1abbff47f58142e5ba61615
Piotr Tarsa <piotr.tarsa@gmail.com>
2017-02-26 19:02:05 +0100

    Printing some statistics in Runner

--- src/test/scala/pl/tarsa/matchfinders/Runner.scala
+++ src/test/scala/pl/tarsa/matchfinders/Runner.scala
@@ -32,28 +32,76 @@ import scala.io.StdIn
 object Runner {
   def main(args: Array[String]): Unit = {
     val data =
-      Files.readAllBytes(Paths.get("corpora", "enwik", "enwik7"))
+      Files.readAllBytes(Paths.get("corpora", "enwik", "enwik8"))
 
     val minMatch = 2
     val maxMatch = 120
 
     val tarsaAcceptedBuilder = mutable.ArrayBuilder.make[Match.Packed]()
-    tarsaAcceptedBuilder.sizeHint(120 * 1000 * 1000)
+    tarsaAcceptedBuilder.sizeHint(210 * 1000 * 1000)
     var tarsaDiscardedCounter = 0
 
     StdIn.readLine("Press Enter to start processing...")
 
-    Timed("TarsaMatchFinder.run") {
-      TarsaMatchFinder.run(data,
-        minMatch,
-        maxMatch,
-        tarsaAcceptedBuilder += _,
-        _ => tarsaDiscardedCounter += 1)
+    for (_ <- 0 to 0) {
+      tarsaAcceptedBuilder.clear()
+      tarsaDiscardedCounter = 0
+      Timed("TarsaMatchFinder.run") {
+        TarsaMatchFinder.run(data,
+                             minMatch,
+                             maxMatch,
+                             tarsaAcceptedBuilder += _,
+                             _ => tarsaDiscardedCounter += 1)
+      }
     }
 
     val tarsaAcceptedArray = tarsaAcceptedBuilder.result()
 
     println(s"Tarsa accepted:  ${tarsaAcceptedArray.length}")
     println(s"Tarsa discarded: $tarsaDiscardedCounter")
+
+    val lengthsHistogram = Array.ofDim[Int](5)
+    for (packedMatch <- tarsaAcceptedArray) {
+      val length = Match(packedMatch).length
+      if (length <= 2) {
+        lengthsHistogram(0) += 1
+      } else if (length <= 5) {
+        lengthsHistogram(length - 2) += 1
+      } else {
+        lengthsHistogram(4) += 1
+      }
+    }
+    println("Accepted matches' lengths histogram:")
+    println(s"  <= 2 - ${lengthsHistogram(0)}")
+    println(s"  == 3 - ${lengthsHistogram(1)}")
+    println(s"  == 4 - ${lengthsHistogram(2)}")
+    println(s"  == 5 - ${lengthsHistogram(3)}")
+    println(s"  >= 6 - ${lengthsHistogram(4)}")
+
+    val offsetsHistogram = Array.ofDim[Int](10)
+    for (packedMatch <- tarsaAcceptedArray) {
+      val offset = Match(packedMatch).offset
+      assert(offset > 0)
+      val log10 = Math.log10(offset)
+      val slot = Math.min(Math.ceil(log10).toInt, 9)
+      offsetsHistogram(slot) += 1
+    }
+    println("Accepted matches' offsets histogram:")
+    println(s"       offset == 1e0 - ${offsetsHistogram(0)}")
+    offsetsHistogram.init.zipWithIndex.tail.foreach {
+      case (counter, logarithm) =>
+        println(s" 1e${logarithm - 1} < offset <= 1e$logarithm - $counter")
+    }
+    println(s" 1e8 < offset        - ${offsetsHistogram(9)}")
+
+    var matchesWithAtLeastLength3AndOffset64KiB = 0
+    for (packedMatch <- tarsaAcceptedArray) {
+      val theMatch = Match(packedMatch)
+      if (theMatch.length >= 3 && theMatch.offset >= (1 << 16)) {
+        matchesWithAtLeastLength3AndOffset64KiB += 1
+      }
+    }
+    println("Accepted matches with length >= 3 and offset >= 64KiB - " +
+      s"$matchesWithAtLeastLength3AndOffset64KiB")
   }
 }
