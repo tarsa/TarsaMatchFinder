5ebb01f6b4c276abfe8fb8b41d1495bc1ff58e1b
Piotr Tarsa <piotr.tarsa@gmail.com>
2017-03-22 23:31:53 +0100

    Added CountingMatchCollector

---
+++ src/main/scala/pl/tarsa/matchfinders/collectors/CountingMatchCollector.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2017 Piotr Tarsa ( http://github.com/tarsa )
+ *
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty.  In no event will the author be held liable for any damages
+ *  arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *  3. This notice may not be removed or altered from any source distribution.
+ *
+ */
+package pl.tarsa.matchfinders.collectors
+import pl.tarsa.matchfinders.model.Match.Packed
+
+class CountingMatchCollector extends MatchCollector {
+  var essentialMatchesCounter: Long = 0
+
+  var discardedMatchesCounter: Long = 0
+
+  override def onAccepted(theMatch: Packed): Unit =
+    essentialMatchesCounter += 1
+
+  override def onDiscarded(theMatch: Packed): Unit =
+    discardedMatchesCounter += 1
+}
